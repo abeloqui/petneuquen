6dee1dbb6579abe87c2e0fdbce26e6a486d08c97
B.André <71408583+abeloqui@users.noreply.github.com>
2026-04-21 19:18:30 -0300

    Add User and Pet models with SQLAlchemy

---
+++ modelos.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default="user")
+    is_verified = Column(Boolean, default=False)
+
+class Pet(Base):
+    __tablename__ = "pets"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    species = Column(String)
+    image_url = Column(String) # Aquí guardaremos el link de Cloudinary
+    owner_id = Column(Integer, ForeignKey("users.id"))
