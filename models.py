ad90b73dd0105dd54a5bc56f969c57320a93921a
B.André <71408583+abeloqui@users.noreply.github.com>
2026-04-21 20:53:48 -0300

    Fix formatting and whitespace in models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     role = Column(String, default="user") # 'admin' o 'user'
-    is_verified = Column(Boolean, default=False) # Solo publican los verificados por vos
+    is_verified = Column(Boolean, default=False)
 
 class Pet(Base):
     __tablename__ = "pets"
@@ -17,9 +17,9 @@ class Pet(Base):
     image_url = Column(String)
     status = Column(String) # 'adopcion' o 'perdido'
     vacunado = Column(Boolean, default=False)
-    vacunas_detalle = Column(String, nullable=True) # Ej: "Rabia, Parvovirus"
+    vacunas_detalle = Column(String, nullable=True)
     lat = Column(Float, nullable=True)
     lon = Column(Float, nullable=True)
-    is_approved = Column(Boolean, default=False) # <--- FILTRO DE ADMIN
+    is_approved = Column(Boolean, default=False) 
     owner_id = Column(Integer, ForeignKey("users.id"))
     
